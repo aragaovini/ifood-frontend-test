0aebb9eba401bb5c6e2b67f1794a4f195ecfa829
Vinicius <disanti2010@gmail.com>
2019-08-01 20:13:13 -0300

    init: inicializando projeto spotifood

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Spotifood
+      </header>
+    </div>
+  );
+}
+
+export default App;
